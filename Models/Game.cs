69413f97808241ffedccaf0c5ae7480e14174b7d
Ben <ben.blanc@georgebrown.ca>
2024-11-02 23:40:28 -0400

    Add project files.

---
+++ Models/Game.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApplicationSports.Models
+{
+    public class Game
+    {
+        [Key]
+        public int GameId {get;set;}
+
+        [Required]
+        public DateTime DateOfMatch{get;set;}
+
+        [ForeignKey("Team")]
+        public int TeamHome { get; set; }
+
+        [Required]
+        public int HomeScore { get; set; }
+        
+        [ForeignKey("Team")]
+        public int TeamAway { get; set; }
+
+        [Required]
+        public int AwayScore { get; set; }
+
+        [ValidateNever]
+        public Team Team1 { get; set; }
+        [ValidateNever]
+        public Team Team2 { get; set; }
+
+    }
+}
---
+++ Models/Team.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationSports.Models
+{
+    public class Team
+    {
+        [Key]
+        public int TeamId { get; set; }
+        [Required]
+        public string TeamName { get; set; }
+        [Required]
+        public string City{ get; set; }
+        [Required]
+        public string Region { get; set; }
+        [Required]
+        public string Country { get; set; }
+        [Required]
+        public string Logo { get; set; }
+        [Required]
+        public string MascotName { get; set; }
+
+
+    }
+}
